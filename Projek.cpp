eab16e991135798925c835e7cf025c9c55a25177
Radika <radikatrimo@gmail.com>
2025-11-07 22:04:54 +0700

    Menambahkan file c++

---
+++ Projek.cpp
@@ -0,0 +1,28 @@
+#include <iosteam>
+#include <string>
+
+using namespace std;
+
+// Bagian 1: Fungsi tambah tugas
+
+
+// BAgian 2: Tampilkan Tugas
+
+
+// Bagian 3: Ubah status tugas
+
+
+// Bagian 4: Hapus tugas
+
+
+// Bagian 5: Fungsi Statistik & menu
+
+
+// Fungsi utama
+int main(){
+
+
+
+
+    return 0;
+}
