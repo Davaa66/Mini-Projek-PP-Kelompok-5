e97ad837da6579f224786bd015060b41da7719e4
Suci Meisyila <sucimeisyila@gmail.com>
2025-11-15 23:17:04 +0700

    Menambahkan fungsi tambah tugas

--- Projek.cpp
+++ Projek.cpp
@@ -13,6 +13,20 @@ int jumlahTugas = 0;
 
 // Bagian 1: Fungsi tambah tugas
 void tambahTugas() {
+    if (jumlahTugas > MAX_TUGAS){
+        cout << "Tugas Penuh" << endl;
+    }else {
+        cout << "Masukan nama tugas: ";
+        getline(cin, namaTugas[jumlahTugas]);
+        cout << "Masukan deadline tugas: ";
+        getline(cin, deadline[jumlahTugas]);
+        cout << "Masukan prioritas tugas (tinggi/sedang/rendah): ";
+        getline(cin, namaTugas[jumlahTugas]);
+
+        statusTugas[jumlahTugas] = false;
+        jumlahTugas++;
+        cout << "Tugas berhasil ditambahkan" << endl;
+    }
 }
 
 // BAgian 2: Tampilkan Tugas
@@ -40,9 +54,11 @@ void tampilkanMenu() {
     cout << "5. Statistik\n";
     cout << "6. Keluar\n";
 }
+
 // Fungsi utama
 int main(){
-    tampilkanMenu();
+    // tampilkanMenu();
+    tambahTugas();
 
     return 0;
 }
